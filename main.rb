7e2967499acc2f0a759c401759b356a5d1fab438
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-01-05 14:25:57 -0500

    Read and print the content of the file

--- main.rb
+++ main.rb
@@ -1,5 +1,25 @@
 class WorldPlay
   
+  # load a dictionary words to an array
+  def load_dictionary(file)
+    
+    # start with and empty array
+    dictionary = []
+    
+    # TODO: Remember to add the exception handler
+    
+      # Open the file
+      f = File.open(file)
+      
+      # Check if the file exists
+      if f
+        
+        # add each line of the file to the dictionary array
+        f.each_line {|line| puts line.strip}
+        
+      end
+  end
 end
 
-
+anagram = WorldPlay.new
+anagram.load_dictionary("files/wl.txt")
