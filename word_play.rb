c1fa43a3021f2fb8c226c74f8bf8b6095a4f3673
Juan Carlos Hazbun Nieto <jhazbun@disetronica.com>
2015-01-06 14:31:34 -0500

    Create function to load dictionary from a file

---
+++ word_play.rb
@@ -0,0 +1,25 @@
+# This class takes a dictionary of words from a file to
+# populates an array of strings. It checks wich set of 
+# words of the dictionary are anagrams and ananagrams.
+#
+# Author::    Juan Carlos Hazbun Nieto <juankhazbun@gmail.com>
+
+class WordPlay
+  
+  # Load the dictionary from a file to an array
+  def load_dictionary(file)
+    
+    dictionary = []
+    
+      # Open the file from the given path
+      f = File.open(file)
+      
+      # Check if the file exists
+      if f
+        
+        # Add each line of the file to the dictionary array
+        f.each_line { |line| dictionary.push line.chomp }
+         
+      end
+  end
+end
